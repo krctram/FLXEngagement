f408ea156f46b3abaa1b2e1d7e88da690c37f388
madhesh <madheshmaasi3@gmail.com>
2021-06-02 18:27:43 +0530

    nishanth design changes june 2

--- src/webparts/flxengagement/FlxengagementWebPart.ts
+++ src/webparts/flxengagement/FlxengagementWebPart.ts
@@ -369,7 +369,7 @@ list.get().then(l => {
             <a data-interception="off" href="${item.URL}" target="_blank"><img class="q-link-img" src="${serverUrl}${imageUrl}" alt="img"/></a><a data-interception="off" class="" href="${item.URL}" target="_blank">
             <div class="q-link-title">${item.Title}</div></a></div>`
           // console.log(items)
-        }
+        }    
         else { 
           html += `<div class = "q-link m-2 text-center p-2"><div class="iconaddengage text-end py-1 px-2">
           <span class="editimageflxengage" data-bs-toggle="modal" data-bs-target="#staticBackdropone" data-id="${item.ID}"></span>
